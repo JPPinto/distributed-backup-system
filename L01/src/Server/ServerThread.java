bb062bfd69bd1182cbc11ab51a0b8987ec2b1d42
Eduardo Fernandes <e.luis@me.com>
2014-02-21 09:37:59 +0000

    _

--- L01/src/Server/ServerThread.java
+++ L01/src/Server/ServerThread.java
@@ -2,7 +2,10 @@ package Server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.*;
 
 /**
  * SDIS Lab 01
@@ -13,29 +16,54 @@ public class ServerThread extends Thread {
 
     protected DatagramSocket socket = null;
     protected BufferedReader in = null;
-    protected boolean serverIsRunning = true;
+    protected boolean serverIsRunning = false;
 
-    public ServerThread() throws IOException{
-        this("Server.ServerThread");
-    }
+    publc ServerThread() {
 
-    publc ServerThread(String name) throws IOException {
-        super(name);
+        try {
+            initializeConnection();
+        } catch (IOException e) {
+            System.out.println("Cannot create server");
+        }
 
-        initializeConnection();
+        serverIsRunning = true;
     }
 
 
 
     public void run(){
         while(serverIsRunning) {
+            try {
+                byte[] buf = new byte[256];
 
+                // receive request
+                DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                socket.receive(packet);
+
+                // figure out response
+                String dString = null;
+                if (in == null)
+                    dString = new Date().toString();
+                else
+                    dString = "Hello";
+
+                buf = dString.getBytes();
+
+                // send the response to the client at "address" and "port"
+                InetAddress address = packet.getAddress();
+                int port = packet.getPort();
+                packet = new DatagramPacket(buf, buf.length, address, port);
+                socket.send(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
+                serverIsRunning = false;
+            }
         }
 
         closeConnection();
     }
 
-    private void initializeConnection(){
+    private void initializeConnection() throws IOException {
         socket = new DatagramSocket(60000);
     }
 
