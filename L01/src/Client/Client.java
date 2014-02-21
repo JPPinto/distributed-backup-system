24d13543fda27e0ca699f00ee72c3372fde1c251
José Pinto <ei12164@fe.up.pt>
2014-02-21 10:11:39 +0000

    Client Complete

--- L01/src/Client/Client.java
+++ L01/src/Client/Client.java
@@ -13,6 +13,8 @@ import java.util.*;
 public class Client {
     public static void main(String[] args) throws IOException {
 
+        Scanner in = new Scanner(System.in);
+
         if (args.length != 1) {
             System.out.println("Usage: java Client ");
             return;
@@ -21,8 +23,15 @@ public class Client {
         // get a datagram socket
         DatagramSocket socket = new DatagramSocket();
 
+        String request = "";
+        if(args[1].equals("REGISTER"))
+            request = args[1] + args[2] + args[3];
+        else
+        if(args[1].equals("LOOKUP"))
+            request = args[1] + args[2];
+
         // send request
-        byte[] buf = new byte[256];
+        byte[] buf = request.getBytes();
         InetAddress address = InetAddress.getByName(args[0]);
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 4445);
         socket.send(packet);
