faa73cec9e401e91779fafbdaa35489b43f24b00
Eduardo Fernandes <e.luis@me.com>
2014-03-15 15:31:19 +0000

    Complete chunk class

--- Project 1/src/Backup/Chunk.java
+++ Project 1/src/Backup/Chunk.java
@@ -1,5 +1,7 @@
 package Backup;
 
+import java.io.Serializable;
+
 /**
  * SDIS TP1
  *
@@ -8,20 +10,48 @@ package Backup;
  *
  * Backup.Chunk class
  */
-public class Chunk {
+
+public class Chunk implements Serializable {
+    private static int chunkDataSize = 64000;
+
     private String fileId;
     private int chunkNo;
     private byte chunkData[];
 
     public Chunk(String fId, int cNo) throws IllegalStateException {
-        if (cNo < 0 || cNo > 999999) {
-            throw new IllegalStateException("Invalid chunk number!");
+        setFileId(fId);
+        setChunkNo(cNo);
+    }
+
+    public Chunk(String fId, int cNo, byte[] data) throws IllegalStateException {
+        setFileId(fId);
+        setChunkNo(cNo);
+        storeData(data);
+    }
+
+    private void setFileId(String fId){
+        if (fId.length() == 64){
+            fileId = fId;
+        } else {
+            throw new IllegalStateException("Invalid file hash!");
         }
 
-        /* Max chunk size 64000 */
+    }
 
-        fileId = fId;
-        chunkNo = cNo;
+    private void setChunkNo(int chunkNoIn){
+        if (chunkNoIn < 0 || chunkNoIn > 999999) {
+            throw new IllegalStateException("Invalid chunk number!");
+        } else {
+            chunkNo = chunkNoIn;
+        }
+    }
+
+    public void storeData(byte[] in){
+        if (in.length <= chunkDataSize) {
+            chunkData = in;
+        } else {
+            throw new IllegalStateException("Data too big for chunk!");
+        }
     }
 
     public int getChunkNo() {
@@ -30,5 +60,9 @@ public class Chunk {
 
     public String getFileId() {
         return fileId;
+    }
+
+    public byte[] getChunkData(){
+        return chunkData;
     }
 }
