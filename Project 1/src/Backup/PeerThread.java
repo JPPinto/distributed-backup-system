c6122788d46354d6f280396ecde5828bdc10faa1
José Pinto <ei12164@fe.up.pt>
2014-03-26 22:52:10 +0000

    Peer can now respond to messages of STORED type. Still need to save chunk to local storage.

--- Project 1/src/Backup/PeerThread.java
+++ Project 1/src/Backup/PeerThread.java
@@ -10,81 +10,85 @@ package Backup;
 
 import java.io.*;
 import java.net.*;
+import java.security.cert.CRL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class PeerThread extends Thread {
 
 	private int mCastPort;
 	private String mCastAddress;
-	private ArrayList<PBMessage> incoming_messages;
+	private SocketReceiver socReceiver;
 
-	private static final int packetSize = 65536;
-
+	public static final int packetSize = 65536;
+	public static final String CRLF = "\r\n";
 
 	PeerThread(int port, String address) {
-		incoming_messages = new ArrayList<PBMessage>();
 		mCastPort = port;
 		mCastAddress = address;
+		socReceiver = new SocketReceiver(address, port);
+
 	}
 
 	public void run() {
+		boolean running = true;
+		socReceiver.run();
 
-		MulticastSocket mSocket = null;
-		InetAddress iAddress;
 
-		try {
-			DatagramPacket packet;
+		while (running) {
 
-			mSocket = new MulticastSocket(mCastPort);
+			for (Map.Entry<String, PBMessage> entry : socReceiver.received_putchunk.entrySet()) {
 
-			/* Join the Multicast Group */
-			iAddress = InetAddress.getByName(mCastAddress);
-			mSocket.joinGroup(iAddress);
+				PBMessage temp_message = entry.getValue();
 
-			while (true) {
-				byte[] buf = new byte[packetSize];
-				packet = new DatagramPacket(buf, buf.length);
+				// TODO Guardar chunk
 
-				// receive the packets
-				mSocket.receive(packet);
+				String temp_request = new String("STORED " + temp_message.version + " " + temp_message.fileId + " " + temp_message.chunkNo + " " + CRLF);
+				sendRequest(temp_request);								// Responde to message
 
-				incoming_messages.add(new PBMessage(packet.getData()));
-
-				if (false) break;
+				socReceiver.received_putchunk.remove(entry.getKey());  	// Remove message from queue
 			}
 
-			mSocket.leaveGroup(iAddress);
-
-		} catch (IOException e) {
-			e.printStackTrace();
-
-			mSocket.close();
 		}
-
-
 	}
 
-	public void sendRequest(String request) throws IOException {
+	public void sendRequest(String request) {
 
 		PBMessage temp_message = new PBMessage(request.getBytes());
-		DatagramSocket socket = new DatagramSocket();
-		InetAddress IPAddress = InetAddress.getByName(mCastAddress);
 
-		if (temp_message.type == PBMessage.PUTCHUNK) {
+		try {
+			DatagramSocket socket = new DatagramSocket();
+			InetAddress IPAddress = InetAddress.getByName(mCastAddress);
 
-			DatagramPacket packet = new DatagramPacket(temp_message.raw_data, temp_message.raw_data.length, IPAddress, mCastPort);
-			socket.send(packet);
+			if (temp_message.type == PBMessage.PUTCHUNK) {
 
-			while (true) {
+				DatagramPacket packet = new DatagramPacket(temp_message.raw_data, temp_message.raw_data.length, IPAddress, mCastPort);
+				socket.send(packet);
 
+				while (true) {
+				/*TO COMPLETE*/
+				}
+
+			} else if (temp_message.type == PBMessage.STORED) {
+
+				DatagramPacket packet = new DatagramPacket(temp_message.raw_data, temp_message.raw_data.length, IPAddress, mCastPort);
+				socket.send(packet);
 			}
 
-		} else if (temp_message.type == PBMessage.STORED) {
+			socket.close();
 
-			DatagramPacket packet = new DatagramPacket(temp_message.raw_data, temp_message.raw_data.length, IPAddress, mCastPort);
-			socket.send(packet);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+	}
+	public static void main (String[] args) throws IOException {
 
-		socket.close();
+		System.out.println("STORED " + 0xDA);
+		BufferedInputStream y = new BufferedInputStream(System.in);
+		y.read();
 	}
 }
+
+
