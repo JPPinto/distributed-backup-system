85882296bf1b8a598661575e1753e9b4e428358b
José Pinto <ei12164@fe.up.pt>
2014-03-27 21:19:23 +0000

    Added several static final.

--- Project 1/src/Backup/PBMessage.java
+++ Project 1/src/Backup/PBMessage.java
@@ -9,6 +9,14 @@ package Backup; /**
 import java.util.regex.Pattern;
 
 abstract class PBMessage {
+
+	//Types of Messages
+	protected static final String PUTCHUNK = "PUTCHUNK";
+	protected static final String STORED = "STORED";
+	protected static final String DELETE = "DELETE";
+	protected static final String CHUNK = "CHUNK";
+	protected static final String GETCHUNK = "GETCHUNK";
+	protected static final String REMOVED = "REMOVED";
     // Constants
     protected static final byte TERMINATOR = (byte) Integer.parseInt("DA", 16);
     protected String version = "1.0";
