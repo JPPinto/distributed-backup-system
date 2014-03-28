511c3c4c85d21c164ea7617f6b0a501d5cd5bea1
Eduardo Fernandes <e.luis@me.com>
2014-03-28 10:33:56 +0000

    Msg_Chunk

--- Project 1/src/Backup/Msg_Chunk.java
+++ Project 1/src/Backup/Msg_Chunk.java
@@ -1,5 +1,10 @@
 package Backup;
 
+import sun.plugin.dom.exception.InvalidStateException;
+
+import java.util.Arrays;
+
+import static Backup.Utilities.convertByteArrayToSring;
 import static Backup.Utilities.joinTwoArrays;
 
 /**
@@ -15,7 +20,9 @@ import static Backup.Utilities.joinTwoArrays;
  */
 public class Msg_Chunk extends PBMessage {
     private byte[] data;
-    //CHUNK <Version> <FileId> <ChunkNo> <CRLF><CRLF> <Body>
+    private byte[] headerData;
+    private byte[] chunkData;
+    int chunkNo;
 
     // Received message constructor
     public Msg_Chunk(byte[] inputData){
@@ -23,6 +30,78 @@ public class Msg_Chunk extends PBMessage {
         receivedMessage = true;
         inputData = data;
 
+        int it = 0;
+        int terminators = 0;
+
+        while (true) {
+            if (it >= inputData.length){
+                throw new InvalidStateException("Message Error!");
+            }
+
+            /* 0xDA */
+            if(inputData[it] == TERMINATOR) {
+                if (terminators == 0){
+                    // -1 ignore space + 0xDA
+                    headerData = Arrays.copyOfRange(inputData, 0, (it - 1));
+                }
+                terminators++;
+            }
+
+            if (terminators == 2) {
+            /* Advance the last terminator */
+                it++;
+                break;
+            }
+
+            it++;
+        }
+
+        String messageHeader = convertByteArrayToSring(headerData);
+
+        // Decode header
+        String[] splitHeader = messageHeader.split(" ");
+
+        if (splitHeader.length == 4){
+            if (!splitHeader[0].equals(PUTCHUNK)){
+                throw new InvalidStateException("Invalid Message!");
+            }
+
+            if(!validateVersion(splitHeader[1])){
+                throw new InvalidStateException("Invalid Message Version!");
+            }
+
+            if(!validateFileId(splitHeader[2])){
+                throw new InvalidStateException("Invalid Message file ID!");
+            }
+
+            if(!validateChunkNo(Integer.parseInt(splitHeader[3]))){
+                throw new InvalidStateException("Invalid Message chunk number!");
+            }
+
+            if(!validateReplicationDeg(Integer.parseInt(splitHeader[4]))){
+                throw new InvalidStateException("Invalid Message replication degree!");
+            }
+
+            version = splitHeader[1];
+            fileId  = splitHeader[2];
+            chunkNo = Integer.parseInt(splitHeader[3]);
+        } else {
+            throw new InvalidStateException("Invalid Message!");
+        }
+
+        // Get the chunk data if it exists
+        if (it == inputData.length || (it + 1) == inputData.length){
+            Chunk receivedChunk = new Chunk(fileId, chunkNo);
+            //receivedChunk.write("pasta");
+
+        } else {
+            // Advance space between 0xDA and the body
+            it++;
+            chunkData = Arrays.copyOfRange(inputData, it, inputData.length);
+
+            Chunk receivedChunk = new Chunk(fileId, chunkNo, chunkData);
+            //receivedChunk.write("pasta");
+        }
     }
 
     // Message to be sent constructor
